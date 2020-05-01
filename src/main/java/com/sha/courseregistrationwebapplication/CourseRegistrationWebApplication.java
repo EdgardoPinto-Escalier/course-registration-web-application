111d7469e36745a65a290b1cadafcc09d1e4b93d
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-05-01 21:45:28 +0200

    Update

--- src/main/java/com/sha/courseregistrationwebapplication/CourseRegistrationWebApplication.java
+++ src/main/java/com/sha/courseregistrationwebapplication/CourseRegistrationWebApplication.java
@@ -1,5 +1,6 @@
 package com.sha.courseregistrationwebapplication;
 
+// Imports
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
